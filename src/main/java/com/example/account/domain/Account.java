f72835d893b6cbb88bfbccca6c4bd2d937a0342e
GyeongSe99 <sgcheon@naver.com>
2023-06-25 04:24:18 +0900

    Account create

--- src/main/java/com/example/account/domain/Account.java
+++ src/main/java/com/example/account/domain/Account.java
@@ -1,7 +1,7 @@
 package com.example.account.domain;
 
+import com.example.account.type.AccountStatus;
 import lombok.*;
-import org.apache.tomcat.jni.Local;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
--- src/main/java/com/example/account/service/AccountService.java
+++ src/main/java/com/example/account/service/AccountService.java
@@ -1,13 +1,20 @@
 package com.example.account.service;
 
 import com.example.account.domain.Account;
-import com.example.account.domain.AccountStatus;
+import com.example.account.domain.AccountUser;
+import com.example.account.exception.AccountException;
 import com.example.account.repository.AccountRepository;
 import com.example.account.repository.AccountUserRepository;
+import com.example.account.type.AccountStatus;
+import com.example.account.type.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+
+import java.time.LocalDateTime;
+
+import static com.example.account.type.AccountStatus.IN_USE;
 
 @Service
 @RequiredArgsConstructor
@@ -21,9 +28,23 @@ public class AccountService {
      * 계좌를 저장하고, 그 정보를 넘긴다.
      */
     @Transactional
-    public void createAccount(Long userId, Long initialBalance) {
-        accountUserRepository.findById(userId)
-                .orElseThrow(() -> new RuntimeException("User Not Found."));
+    public Account createAccount(Long userId, Long initialBalance) {
+        AccountUser accountUser = accountUserRepository.findById(userId)
+                .orElseThrow(() -> new AccountException(ErrorCode.USER_NOT_FOUND));
+
+        String newAccountNumber = accountRepository.findFirstByOrderByIdDesc()
+                .map(account -> (Integer.parseInt(account.getAccountNumber())) + 1 + "")
+                .orElse("1000000000");
+
+        return accountRepository.save(
+                Account.builder()
+                .accountUser(accountUser)
+                .accountStatus(IN_USE)
+                .accountNumber(newAccountNumber)
+                .balance(initialBalance)
+                .registeredAt(LocalDateTime.now())
+                .build()
+        );
     }
 
     @Transactional
